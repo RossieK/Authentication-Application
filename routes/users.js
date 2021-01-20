a9850711079f22b288431250406f35cabebfe4e7
RossieK <rosi.kirova96@gmail.com>
2021-01-20 21:47:01 +0200

    Add register template

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 //Login page
-router.get('/login', (req, res) => res.send('Login'));
+router.get('/login', (req, res) => res.render('Login'));
 
 //Register page
-router.get('/register', (req, res) => res.send('Register'));
+router.get('/register', (req, res) => res.render('Register'));
 
 module.exports = router;
