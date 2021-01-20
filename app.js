1b60baf1f13fbb220db0ede236a7213bc1c727d9
RossieK <rosi.kirova96@gmail.com>
2021-01-20 22:16:22 +0200

    Add MongoDB connection

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const path = require('path');
+const mongoose = require('mongoose');
 
 const app = express();
+
+//DB config
+const db = require('./config/keys').MongoURI;
+
+//Connect to Mongo
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.error(err));
 
 //EJS
 app.use(expressLayouts);
